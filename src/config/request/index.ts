877a5cfd792085189255311463d3aee373e28f84
Brian Liu <74143431+Kele-Bingtang@users.noreply.github.com>
2023-04-15 19:35:17 +0800

    feat: 🚀 views 组件支持非根节点，request 内容修改

--- src/config/request/index.ts
+++ src/config/request/index.ts
@@ -211,8 +211,8 @@ function processArray(config: InternalAxiosRequestConfig) {
  * @returns 持久化数据
  */
 function processError(error: AxiosError) {
-  const e = JSON.stringify(error);
-  if (e !== "{}") {
+  const e = JSON.parse(JSON.stringify(error));
+  if (Object.keys(e).includes("baseURL")) {
     const {
       config: { baseURL, url, params, method, data },
     } = JSON.parse(e);
