dff7bfa265a872b607ad95fe418503fd7bdbb4fc
kele_Bingtang <2456019588@qq.com>
2023-11-24 23:49:18 +0800

    feat: 🚀 修复不开启错误日志捕获导致错误无法输出控制台问题，修改文件名，符合命名规范

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import I18n from "@/languages/index";
 // svg icons
 import "virtual:svg-icons-register";
 import Icon from "@/components/Icon/index.vue";
-import errorHandler from "@/utils/layout/errorHandler";
+import errorHandler, { checkNeed } from "@/utils/layout/errorHandler";
 import Auth from "@/components/Permission/auth";
 import Role from "@/components/Permission/role.vue";
 
@@ -21,7 +21,7 @@ const pinia = createPinia();
 const app = createApp(App);
 pinia.use(piniaPluginPersistedstate);
 
-app.config.errorHandler = errorHandler;
+checkNeed() && (app.config.errorHandler = errorHandler);
 
 // 全局注册按钮级别权限组件
 app.component("Auth", Auth);
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>测试</div>
+  <div>
+    <el-link type="primary" href="https://vue3-docs.youngkbt.cn/">使用文档</el-link>
+  </div>
 </template>
 
 <script setup lang="ts" name="Home"></script>
